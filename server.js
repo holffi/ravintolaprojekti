9a5f9f32f5731055229e007a16dd086b2db6d21e
= <=>
2024-12-10 13:01:04 +0200

    testi

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const app = express();
 
 const port = 3000;
 const host = 'localhost';
-
+app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'sivupohjat'));
 
 app.get('/', (req, res) =>{
-    res.send("Moi")
+    const tervehdys = "MOIKKA TIIMILÄISET"
+    res.render('index',{ tervehdys:tervehdys});
 });
 
 app.listen(port, host, () => console.log(`${host}:${port} kuuntelee...`));
