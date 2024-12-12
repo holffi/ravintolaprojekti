9125ea672884acacccf082a632df16ccf9ad79cb
holffi <lenni.kylmaniemi@gmail.com>
2024-12-12 22:09:40 +0200

    pöydät valmis

--- db.js
+++ db.js
@@ -7,5 +7,25 @@ const db = new sqlite3.Database(tietokanta, function (virhe) {
     console.error(virhe.message);
   } else {
     console.log('yhteystietokantaan saatu');
+    // Pöydät taulu
+    db.run(
+      `CREATE TABLE pöydät (
+        pöytä_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        koko INTEGER
+        )`,
+      function (virhe) {
+        if (virhe) {
+          console.log('taulu on jo tehty');
+        } else {
+          const lisää = 'INSERT INTO pöydät (koko) VALUES (?)';
+          db.run(lisää, [4]);
+          db.run(lisää, [4]);
+          db.run(lisää, [4]);
+          db.run(lisää, [6]);
+          db.run(lisää, [2]);
+        }
+      }
+    );
   }
 });
+module.exports = db;
